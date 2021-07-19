847edc78db27e60cd793798d1d0e3692b1654797
Kamogelo Mkonto <82159075+Kamou01@users.noreply.github.com>
2021-07-19 14:42:01 +0200

    Add files via upload

---
+++ http_methods/app5.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+app=Flask(__name__)
+
+
+@app.route('/')
+def log():
+    return render_template('login.html')
+
+
+@app.route('/login',methods=['POST'])
+def login():
+    uname = request.form['username']
+    password = request.form['userpass']
+    if uname == "kamogelo" and password == "1234":
+        return "Welcome %s" % uname + request.method
+    else:
+        return "Error in logging in" + request.method
+
+
+if __name__=='__main__':
+    app.run(debug=True)
